5f45abf66db6db9403faae1d92579d06f0303e6e
ksrcr7 <kasrabakhshaei25@gmail.com>
2026-08-12 11:23:27 +0330

    Refactor console helper functions

---
+++ include/ConsoleUtils.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <sqlite3.h>
+#include "Book.h"
+
+
+
+void printMenu();
+int getCommandInput();
+sqlite3_int64 getIdInput();
+sqlite3_int64 getValidIdInput(const std::vector<Book>& books);
+Book getBookbyId(const std::vector<Book>& books, sqlite3_int64 id);
+void editBookData(Book& book);
+void printSearchResults(const std::vector<Book>& books);
